77396c222f504244d71475d5d16614391db8c21e
repoteevo <48951938+repoteevo@users.noreply.github.com>
2019-03-26 10:12:32 -0300

    Update Create default RSV.ps1
    Including comments for help.

--- Create default RSV.ps1
+++ Create default RSV.ps1
@@ -5,10 +5,9 @@ $CUSTOMER_FULL = "SKA"
 ##### Preparing variables ##########################################
 $LOCATION = "East US 2"
 $RSV_NAME = "RSV-" + $CUSTOMER_FULL
-$RSG_BACKUP = "GRPRD-" + $CUSTOMER_FULL + "-BACKU"
+$RSG_BACKUP = "GRPRD-" + $CUSTOMER_FULL + "-BACKUP"
 $PLAN_NAME = "PLAN-BKP-" + $CUSTOMER_FULL +"-Default"
 ####################################################################
-
 
 #### Validating Resource Group ####
 $RSG = Get-AzureRmResourceGroup -Name $$RSG_BACKUP -Location $LOCATION
@@ -17,9 +16,11 @@ if (!$?) {
     $RSG = New-AzureRmResourceGroup -Name $$RSG_BACKUP -Location $LOCATION
 }
 
+#### Creating Recovery Services Vault 
 $RSV =  New-AzureRmRecoveryServicesVault -Name $RSV_NAME  -ResourceGroupName $RSG.ResourceGroupName  -Location $LOCATION
 Set-AzureRmRecoveryServicesBackupProperties -Vault $RSV -BackupStorageRedundancy LocallyRedundant
 
+#### Preparing next backup time
 $TODAY = Get-Date
 $KIND = new-object System.DateTimeKind
 $KIND.value__ = 1 # UTC
@@ -28,6 +29,7 @@ $DATE = New-Object system.datetime($TODAY.Year,$TODAY.Month,$TODAY.Day,18,00,00,
 $SCHED 		= Get-AzureRmRecoveryServicesBackupSchedulePolicyObject -WorkloadType AzureVM -BackupManagementType AzureVM
 $SCHED.ScheduleRunTimes[0] = $DATE
 
+#### Setting retentions
 $RETENTION 	= Get-AzureRmRecoveryServicesBackupRetentionPolicyObject -WorkloadType AzureVM -BackupManagementType AzureVM
 $RETENTION.IsYearlyScheduleEnabled = $false
 $RETENTION.DailySchedule.DurationCountInDays = 15
@@ -37,4 +39,5 @@ $RETENTION.MonthlySchedule.RetentionScheduleFormatType = 'Daily'
 $RETENTION.MonthlySchedule.RetentionScheduleDaily[0].DaysOfTheMonth[0].Date = 0
 $RETENTION.MonthlySchedule.RetentionScheduleDaily[0].DaysOfTheMonth[0].isLast = $true
 
+#### Creating default backup policy
 New-AzureRmRecoveryServicesBackupProtectionPolicy -Name $PLAN_NAME -WorkloadType AzureVM -BackupManagementType AzureVM -RetentionPolicy $RETENTION -SchedulePolicy $SCHED -VaultId $RSV.ID
